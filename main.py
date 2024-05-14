4984e464e8f24e20b60be90c26cf9c624d835db4
SayliV <sayliv.1124244@gmail.com>
2024-05-14 12:34:53 +0800

    Work with other def and users.

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+from index import *
+
+file_path = input("Enter file path:")
+def create_EXCEL_log_tabel ():
+
+    print(extract_data_from_file(file_path))
+
+
+
+#C:\Users\lenovoPC\PycharmProjects\extracted_data_to_exel\2019.txt
